1faeb924393faae814c0cbfb0cb2a5d6e2fc00a5
Olivier <olivier.metzinger@epitech.eu>
2019-09-14 22:41:09 +0900

    change

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Update()
+    {
+        if (!isLocalPlayer) {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        //Création de la balle à partir de la prefab "Bullet"
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Ajout de velocité à la balle
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+        //Faire apparaitre la balle sur les clients
+        NetworkServer.Spawn(bullet);
+
+        // Destruction de la balle après 2 secondes
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
